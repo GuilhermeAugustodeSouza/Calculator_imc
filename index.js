3fff10f9a8acc4a762eb85c1dcaf66e7d5398057
Guilherme Augusto de Souza Goudinho <99102561+GuilhermeAugustodeSouza@users.noreply.github.com>
2023-01-04 20:23:07 -0300

    Update index.js
    added code and comuted.

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -24,10 +23,25 @@
         }
 
         var nome= prompt("Digite seu nome :");
-        var altura= prompt(nome +"Digite sua altura :");
-        var peso= prompt(nome + "Digite seu peso :");
-        var imc= calculaImc(altura,peso)
-         mostra("Seu Imc é "+ Math.round( imc));
+        var altura= prompt(nome + " digite sua altura :");
+        var peso= prompt(nome + " digite seu peso :");
+        var imc= calculaImc(altura,peso);
+         mostra("Seu Imc é  "+ Math.round( imc)+"!");
+         if (imc <= 18.5 ){
+             mostra("Você esta muito magro!");
+            }else if (imc >=18.5 && imc <= 24.9){
+                 mostra("Peso normal !");
+             }else if(imc >= 25 && imc <= 29.9){
+                 mostra("Você está com sobrepeso!");
+             }else if(imc >= 30 && imc <=34.9){
+                 mostra("Você está com obesidade grau I.");
+             }else if(imc >= 35 && imc <= 40){
+                 mostra("Você está com obesidade grau II.");
+             }else if (imc >40){
+                 mostra("Você está com obesidade grau III,cuide-se!!");
+             }
+
+              
 
 
 
